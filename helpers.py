3d7f2546f9626be08679034f5b435ec56298ce84
EC2 Default User <ec2-user@ip-172-16-62-23.us-west-2.compute.internal>
2021-03-29 06:54:34 +0000

    sunday updates

--- helpers.py
+++ helpers.py
@@ -43,19 +43,36 @@ def remove_punc(s):
 
 
 class Proctologist:
-
     def __init__(self):
         self.cv = pickle.load(open('models/cv_fit_train.sav', 'rb'))
-        self.pca = pickle.load(open('models/pca.sav', 'rb'))
-
+        self.pca = pickle.load(open('models/pca_combo.sav', 'rb'))
+        self.model = pickle.load(open('models/lr.sav', 'rb'))
+        
+    def text_convert(self, arr):
+        text_df = pd.DataFrame(arr, columns = ['text'])
+        # clean up the text
+        text_df['text_lil_clean'] = text_df['text'].apply(lambda x : clean_lil_bit(x))
+        # add sentiment analysis score to df
+        text_df['polarity'] = text_df['text_lil_clean'].apply(lambda x : TextBlob(x).sentiment.polarity)
+        text_df['subjectivity'] = text_df['text_lil_clean'].apply(lambda x : TextBlob(x).sentiment.subjectivity)
+        
+        # fincal clean up of text
+        text_df['text_clean'] = text_df['text_lil_clean'].apply(lambda x: remove_punc(x))
+        
+        self.df = text_df[['text_clean','polarity','subjectivity']]
+        
+        # create dtm DF
+        cv_dtm = self.cv.transform(self.df['text_clean'])
+        cv_cols = self.cv.get_feature_names()
+        self.dtm = pd.DataFrame(cv_dtm.toarray(), columns=cv_cols)
+        
+        dtm_pca = self.pca.transform(self.dtm)
+        pca_cols = ['PC_' + str(i) for i in range(1, self.pca.get_params()['n_components']+1)]
+        self.dtm_pca_df = pd.DataFrame(dtm_pca, columns=pca_cols)
     
-
-    def text_convert(s):
-        s_clean = clean_lil_bit(s)
-        s_clean = remove_punc(s_clean)
-
-        dtm_s = cv.transform(s_clean)
-        dtm_pca = pca.transform(dtm_s)
-        return dtm_pca
-
+        self.X = pd.concat([self.df[['polarity','subjectivity']], self.dtm_pca_df], axis=1)
+        
+    def judgement(self, arr):
+        self.text_convert(arr)
+        return self.model.predict(self.X)
 
